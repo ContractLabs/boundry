ff880b490877c373c2625346318317245445e4b8
tasiby <tuanhoang.uit@gmail.com>
2023-09-25 16:54:02 +0700

    remove sh

--- sh/script.sh
+++
@@ -1,2 +0,0 @@
-source .env
-forge script script/counter/CounterScript.s.sol --rpc-url $MUMBAI --etherscan-api-key $POLYGONSCAN_API_KEY --broadcast --verify --legacy --ffi -vvvv
--- sh/setup.sh
+++
@@ -1,6 +0,0 @@
-forge install --no-commit PaulRBerg/prb-math
-forge install --no-commit foundry-rs/forge-std
-forge install --no-commit transmissions11/solmate
-forge install --no-commit openzeppelin/openzeppelin-contracts
-forge install --no-commit openzeppelin/openzeppelin-contracts-upgradeable
-forge install --no-commit vectorized/solady
