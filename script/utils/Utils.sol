5cc3ed710a830425ddee17eb56532e2a116ba648
tasiby <tuanhoang.uit@gmail.com>
2023-09-28 13:27:20 +0700

    temporary deployment log

--- script/utils/Utils.sol
+++ script/utils/Utils.sol
@@ -2,15 +2,36 @@
 pragma solidity 0.8.20;
 
 import { Script, console2 } from "forge-std/Script.sol";
+import { JSONParserLib } from "solady/src/utils/JSONParserLib.sol";
 
 contract UtilsScript is Script {
+    using JSONParserLib for *;
+
     mapping(string => bool) __madeDir;
+    mapping(uint256 => string) __chainName;
     mapping(uint256 => mapping(address => bool)) __storageLayoutGenerated;
+
+    function setUp() public {
+        __chainName[1] = "ethereum";
+        __chainName[5] = "goerli";
+        __chainName[43_113] = "fuji";
+        __chainName[43_113] = "avalanche";
+        __chainName[137] = "polygon";
+        __chainName[80_001] = "mumbai";
+        __chainName[56] = "binance-mainnet";
+        __chainName[97] = "binance-testnet";
+        __chainName[42_161] = "arbitrum-mainnet";
+        __chainName[421_613] = "arbitrum-testnet";
+    }
+
+    function run() public virtual {
+        _deploymentLog(0xf543747650D81042FE61BE4Fe861311D1865C8e8, "CounterUpgradeable", 97);
+    }
 
     function _storageLayoutLog(string memory contractName, address implement, uint256 chainId) internal {
         if (__storageLayoutGenerated[chainId][implement]) return;
 
-        _mkdir(_basePath(string.concat("storage/", vm.toString(chainId))));
+        _mkdir(_basePath(string.concat("storage/", __chainName[chainId])));
 
         string[] memory script = new string[](5);
         script[0] = "forge";
@@ -23,6 +44,56 @@ contract UtilsScript is Script {
         vm.writeFile(_getStorageLayoutPath(implement, chainId), string(out));
 
         __storageLayoutGenerated[chainId][implement] = true;
+    }
+
+    function _getContractDataByKey(string memory contractName, string memory key) internal returns (string memory) {
+        string[] memory script = new string[](4);
+        script[0] = "forge";
+        script[1] = "inspect";
+        script[2] = contractName;
+        script[3] = key;
+        bytes memory out = vm.ffi(script);
+        return string(out);
+    }
+
+    function _deploymentLog(address contractAddress, string memory contractName, uint256 chainId) internal {
+        string memory jsonData;
+        string memory filePath;
+        string memory abiContract = _getContractDataByKey(contractName, "abi");
+        string memory metadata = _getContractDataByKey(contractName, "metadata");
+        string memory storageLayout = _getContractDataByKey(contractName, "storage-layout");
+
+        _mkdir(_basePath(__chainName[chainId]));
+        filePath = _getDeploymentsLogPath(contractName, chainId);
+        jsonData = _baseJsonFormat(contractAddress, metadata, abiContract, storageLayout);
+
+        vm.writeJson(jsonData, filePath);
+    }
+
+    function _baseJsonFormat(
+        address addr,
+        string memory metadata,
+        string memory abiContract,
+        string memory storageLayout
+    )
+        internal
+        pure
+        returns (string memory)
+    {
+        return string.concat(
+            '{"address": "',
+            vm.toString(addr),
+            '",',
+            '"storage": ',
+            storageLayout,
+            ",",
+            '"abi": ',
+            abiContract,
+            ",",
+            '"metadata": ',
+            metadata,
+            "}"
+        );
     }
 
     function _collisionCheck(
@@ -126,10 +197,21 @@ contract UtilsScript is Script {
         return string.concat("deployments/", path);
     }
 
-    function _getStorageLayoutPath(address implementation, uint256 chainId) internal pure returns (string memory) {
+    function _getStorageLayoutPath(address implementation, uint256 chainId) internal view returns (string memory) {
         return _basePath(
-            string.concat("storage/", vm.toString(chainId), "/", vm.toString(implementation), ".storage-layout")
+            string.concat("storage/", __chainName[chainId], "/", vm.toString(implementation), ".storage-layout")
         );
+    }
+
+    function _getDeploymentsLogPath(
+        string memory contractName,
+        uint256 chainId
+    )
+        internal
+        view
+        returns (string memory)
+    {
+        return _basePath(string.concat(__chainName[chainId], "/", contractName, ".json"));
     }
 
     function _getTemporaryStorageLayoutPath(address implementation) internal pure returns (string memory) {
