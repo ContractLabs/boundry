f14dcd9dd4b10347293f577878c7ed608c22b040
tasibii <tasibii.web3@gmail.com>
2024-02-02 09:59:42 +0700

    add conflict

--- src/Sample.sol
+++ src/Sample.sol
@@ -5,6 +5,7 @@ import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Ini
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract Sample {
+  address conflict;
   string public greeting;
 
   constructor() {
